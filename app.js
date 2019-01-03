6806bcde0afe420c04f2388bd1a1849f782ef58c
Angela Larryant <angela@Angelas-MacBook-Pro.local>
2019-01-03 15:44:09 -0800

    fixed spacing

--- app.js
+++ app.js
@@ -3,38 +3,4 @@ $(document).ready(function(){
     event.preventDefault();
     window.print();
   });
-
-  // $('.fa-camera').mouseover(function(event) {
-  //   event.preventDefault();
-  //   $targetDescription = $(event.target).next();
-  //   $targetDescription.slideDown(2000);
-  // });
-
-  // $('.fa-gamepad').mouseover(function(event) {
-  //   event.preventDefault();
-  //   $targetDescription = $(event.target).next();
-  //   $targetDescription.slideDown(2000);
-  //   // $targetDescription.slideUp();
-  // });
-
-  // $('.fa-palette').mouseover(function(event) {
-  //   event.preventDefault();
-  //   $targetDescription = $(event.target).next();
-  //   $targetDescription.slideDown(2000);
-  //   // $targetDescription.slideUp();
-  // });
-
-  // $('.fa-plane').mouseover(function(event) {
-  //   event.preventDefault();
-  //   $targetDescription = $(event.target).next();
-  //   $targetDescription.slideDown(2000);
-  //   // $targetDescription.slideUp();
-  // });
-
-  // $('.fa-cookie-bite').mouseover(function(event) {
-  //   event.preventDefault();
-  //   $targetDescription = $(event.target).next();
-  //   $targetDescription.slideDown(2000);
-  //   // $targetDescription.slideUp();
-  // });
 });
